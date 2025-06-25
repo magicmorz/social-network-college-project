9ea6c78c4300fc70e5e1d401f01ab91bea9f3ebf
DavidHoch <davidhochner1@gmail.com>
2025-06-25 20:46:32 +0300

    add authentication control functionality

---
+++ Controllers/authController.js
@@ -0,0 +1,49 @@
+const User = require('../models/User');
+
+// GET login page
+exports.getLogin = (req, res) => {
+    res.render('auth/login');
+};
+
+// POST login form
+exports.postLogin = async (req, res) => {
+    const { username, password } = req.body;
+    const user = await User.findOne({ username });
+
+    if (!user || !(await user.comparePassword(password))) {
+        return res.status(401).send("Invalid login");
+    }
+
+    req.session.userId = user._id;
+    res.redirect('/home');
+};
+
+// GET register page
+exports.getRegister = (req, res) => {
+    res.render('auth/register');
+};
+
+// POST register form
+exports.postRegister = async (req, res) => {
+    const { username, email, password } = req.body;
+
+    try {
+        const existingUser = await User.findOne({ username });
+        if (existingUser) {
+            return res.status(400).send("Username already exists");
+        }
+
+        await new User({ username, email, password }).save(); // password is auto-hashed
+        res.redirect('/auth/login');
+    } catch (err) {
+        console.error("❌ Registration failed:", err);
+        res.status(500).send("Registration error");
+    }
+};
+
+// GET logout
+exports.logout = (req, res) => {
+    req.session.destroy(() => {
+        res.redirect('/auth/login');
+    });
+};
